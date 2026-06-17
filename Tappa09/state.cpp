647855b3dcb1bb06247ebd05afce7c0563439da6
gbrl-02 <gabri02renna@gmail.com>
2026-06-17 12:15:18 +0200

    Create mappe che prendono in considerazione le flag della progressione.

--- Tappa09/state.cpp
+++ Tappa09/state.cpp
@@ -375,6 +375,17 @@ void State::reset()
     gameMode = PLAYING;
     roomname = room1;
 
+    bool chest_3_opened = false;
+    bool chest_5_opened = false;
+    bool chest_6_opened = false;
+
+    bool room_3_gauntlet_triggered = false;
+    bool room_3_gauntlet_cleared = false;
+    bool room_5_gauntlet_triggered = false;
+    bool room_5_gauntlet_cleared = false;
+    bool room_6_gauntlet_triggered = false;
+    bool room_6_gauntlet_cleared = false;
+
     room.unload();
     room.load(roomname);
 
